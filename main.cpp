a09fea873fe8421deb4d387df666ee7202236bd5
“kvnalvz” <kevin_32297@yahoo.com>
2021-04-12 19:19:02 -0700

    added more to main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,15 @@ using namespace std;
 
 int main()
 {
-    cout << "hello!" << endl;
+    int choice1;
+    cout << "Welcome to 862131623 8 puzzle solver." << endl;
+
+    cout << "Type \"1\" too use a default puzzle, or \"2\" to enter your own puzzle." << endl;
+    cin >> choice1;
+
+
+    cout << "Enter your choice of algorithm." << endl; 
+    cout << "\nUniform Cost Search\nA* with the Misplaced Tile heuristic\nA* with the Eucledian distance heuristic\n";
 
 
     return 0;
